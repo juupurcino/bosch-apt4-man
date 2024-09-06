07b92f7e867e1557e18059868046e2852d1cf27b
juupurcino <jdiaspurcino@gmail.com>
2024-09-06 15:11:36 -0300

    API pronta

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Card } from './components/Card'
+import { CardAPI } from './components/CardAPI'
 import produtos from './constants/produtos.json'
 import { api } from "./api/rmApi"
 import style from './App.module.css'
@@ -8,6 +9,7 @@ function App() {
   const [show, setShow] = useState("")
   const [data, setData] = useState([])
   const [page, setPage] = useState("")
+  const [name, setName] = useState("")
 
 
   useEffect(() => {
@@ -18,11 +20,25 @@ function App() {
       setData(response.data.results)
     }).catch((error) => {
       if(error.response.status === 404){
-        console.log("Esta pagina nao contem este personagem")
+        alert("Esta pagina nao contem este personagem")
       }
       console.error(error)
     })
   }, [page])
+
+  useEffect(() => {
+    api.get(`/character/?name=${name}`).then((response) => {
+      if(!response.data.results){
+        console.log("Vazio")
+      }
+      setData(response.data.results)
+    }).catch((error) => {
+      if(error.response.status === 404){
+        alert("Esta pagina nao contem este personagem")
+      }
+      console.error(error)
+    })
+  }, [name])
 
   return (
     <>
@@ -52,10 +68,13 @@ function App() {
                <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)}/>
             </div>
             <div>
+               <input type="text" placeholder="Digite um nome" value={name} onChange={(event) => setName(event.target.value)}/>
+            </div>
+            <div className={style.config}>
             {data.map((item) => { 
              return(
-              <div key={item.id}>
-                <Card name={item.name} desc={item.species} value={item.gender} image={item.image} />
+              <div key={item.id} className={style.card}>
+                <CardAPI  name={item.name} desc={item.species} value={item.gender} image={item.image} species={item.species} type={item.type} gender={item.gender}/>
                 {/* <button onClick={() => {}}>Info</button> */}
               </div>
               )
---
+++ src/components/CardAPI.jsx
@@ -0,0 +1,15 @@
+import styleCard from './Card.module.css'
+
+/* eslint-disable react/prop-types */
+export const CardAPI = ({name, status, species, type, gender, image}) => {
+  return(
+      <div className={styleCard.card}>
+          <h1>{name}</h1>
+          <img src={image} alt={name} width={150} height={"auto"}/>
+          <h2>{species}</h2>
+          <p>{type}</p>
+          <p>{gender}</p>
+          <p className={status ? styleCard.true : styleCard.false }>O</p>
+      </div>
+  )
+}
